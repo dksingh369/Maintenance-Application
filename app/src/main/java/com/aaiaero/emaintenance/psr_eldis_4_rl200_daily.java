ad16574a50cba524c7170c00d2a8dd2a32d91da2
tathuru <tathuru9866@gmail.com>
2022-10-13 17:37:49 +0530

    commit for syncing laptop project with project pc , don't pull this commit . 2

---
+++ app/src/main/java/com/aaiaero/emaintenance/psr_eldis_4_rl200_daily.java
@@ -0,0 +1,14 @@
+package com.aaiaero.emaintenance;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class psr_eldis_4_rl200_daily extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_psr_eldis4_rl200_daily);
+    }
+}
