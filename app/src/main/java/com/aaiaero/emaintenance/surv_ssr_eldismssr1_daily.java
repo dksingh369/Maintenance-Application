29f6da950d0f6327c089f9d9ccb8be4719545caa
tathuru <tathuru9866@gmail.com>
2022-11-21 09:07:24 +0530

    mssr daily eldis 4 xml developing

---
+++ app/src/main/java/com/aaiaero/emaintenance/surv_ssr_eldismssr1_daily.java
@@ -0,0 +1,14 @@
+package com.aaiaero.emaintenance;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class surv_ssr_eldismssr1_daily extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_surv_ssr_eldismssr1_daily);
+    }
+}
